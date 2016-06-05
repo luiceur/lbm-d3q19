a6f488479f75eb542be6748fe6b77e83658d0ef5
Luis Cebamanos <l.cebamanos@epcc.ed.ac.uk>
2016-06-05 23:17:10 +0100

    paremeters files to split the main code

---
+++ parameters.c
@@ -0,0 +1,66 @@
+#include "parameters.h"
+
+// 3D vector
+/* Struct F3{ */
+/*   Float x; */
+/*   Float y; */
+/*   Float z; */
+/* }; */
+
+/* struct F3{ */
+/*   double x; */
+/*   double y; */
+/*   double z; */
+/* }; */
+
+
+
+/* struct LB{
+  
+/*   // Number of dimensions */
+/*   const int n = 3; */
+  
+/*   // Grid dims */
+/*   //const unsigned int nx = 3; */
+/*   //const unsigned int ny = 6; */
+/*   //const unsigned int nz = 3; */
+/*   const unsigned int nx = 37; */
+/*   const unsigned int ny = 37; */
+/*   const unsigned int nz = 37; */
+  
+/*   // Grid cell width */
+/*   const Float dx = 1.0; */
+  
+/*   // Number of flow vectors in each cell */
+/*   const int m = 19; */
+  
+/*   // Time step length */
+/*   //const double dt = 1.0; */
+/*   const double dt = 1.0e-3; */
+/*   //const double dt = 1.0e-6; */
+
+/*   // Simulation end time */
+/*   //const Float t_end = 1.5e-4; */
+/*   const double t_end = 2.0; */
+/*   //const double t_end = 1.0; */
+/* //const Float t_end = 10.1; */
+  
+/*   const double t_file = 0.01; */
+  
+/*   // Fluid dynamic viscosity */
+/*   const Float nu = 8.9e-4; */
+  
+/*   // Gravitational acceleration */
+/*   //const Float3 g = {0.0, 0.0, -10.0}; */
+/*   const Float3 g = {0.0, 0.0, 0.0}; */
+  
+/*   // Initial cell fluid density (dimensionless) */
+/*   const Float rho0 = 1.0; */
+  
+/*   // Inital cell fluid velocity (dimensionless) */
+/*   const Float3 u0 = {0.0, 0.0, 0.0}; */
+  
+/*   // Courant criteria limit */
+/*   const Float C_max = 1.0; */
+  
+/* }; */
---
+++ parameters.h
@@ -0,0 +1,14 @@
+#ifndef _PARAMETERS_H
+#define _PARAMETERS_
+
+/* Floating point precision */
+typedef double Float; 
+
+/* 3D vector */
+typedef struct F3{
+  double x;
+  double y;
+  double z;
+}Float3;
+
+#endif
